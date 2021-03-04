28dc2ac1bd8ff114444e226b414587342094d2dd
Lady Gaga <jessiezheqi.li@gmail.com>
2021-03-04 13:13:59 -0800

    Add color coding to all timeblocks

--- script.js
+++ script.js
@@ -2,11 +2,32 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
 
 //step 3.2: make input bg change according to past,current & upcoming block
+var now = moment().format("HH");
+console.log(now);
+
+// reference time-block, i.e. take all 9 timeblocks' textarea using jQuery $('text-area') or .time-block
+var allBlocks = $(".time-block");
+
+for (var i = 0; i < 9; i++) {
+    //console.log(allBlocks.index(i));
+
+    var oneBlock = allBlocks[i];
+    //console.log(oneBlock);
+    //console.log(oneBlock.dataset.hour);//.dataset works! it seems that the jQuery wrapper on $(".time-block") doesn't get recognized after being assigned to variable, so .data('hour') gives error "... .data() is not a function"
+    //console.log(allBlocks[i].dataset.hour);
+    if (oneBlock.dataset.hour < now) {//tried to add (... || (oneBlock.dataset.hour === 9)) but still showed 0th index data is not smaller than present time
+        oneBlock.setAttribute('class','form-control time-block bg-primary');//since not recognizing the jQuery wrapper, need to also not use attr() but setAttribute
+    } else if (oneBlock.dataset.hour === now) {
+        oneBlock.setAttribute('class','form-control time-block bg-warning');
+    }  else {
+        oneBlock.setAttribute('class','form-control time-block bg-danger');
+    }
+}
+
 
 //step 4.1: add localStorage. Select input using jQuery.
-var inputEl = $('input[name="first-block"]');
-//var inputFirstBlock = inputEl.val();//similar to .value in JS, but doesn't log through a variable
-//console.log(inputFirstBlock);
+var inputEl = $('input[name="first-block"]');//want to use textarea tag instead of input tag (used in forms) to allow for for larger content
+
 $('button[name="first-event"]').on("click", function(event) {
     event.preventDefault();
     //console.log("First event", inputEl.val());//somehow only logs on the element.val()
